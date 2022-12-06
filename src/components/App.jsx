cca4b9730ac24e05044646b0b94ebee50ca88acc
SerhiiSamara <ndaair@gmail.com>
2022-12-06 14:14:07 +0200

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import { GlobalStyle } from './GlobalStyle';
+import 'modern-normalize';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+			<p>Hello</p>
+    </>
   );
 };
